ac224211b8e19559fb3f4c50990ff6e40754cd7f
julip <prietojulian2003@gmail.com>
2023-02-06 19:09:19 -0300

    All Possible Moves 1/2

--- chess.py
+++ chess.py
@@ -3,6 +3,9 @@ import numpy as np
 
 class GameState:
     def __init__(self):
+        """
+        estado actual del juego
+        """
         self.board = np.array([
             ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"],
             ["bP", "bP", "bP", "bP", "bP", "bP", "bP", "bP"],
@@ -17,32 +20,112 @@ class GameState:
         self.move_log = []
 
     def make_move(self, move):
+        """
+        Hacer un movimiento
+        :param move:
+        """
         self.board[move.start_r][move.start_c] = "--"
         self.board[move.end_r][move.end_c] = move.piece_moved
         self.move_log.append(move)
         self.white_turn = not self.white_turn
 
-    def undo_move(self):pass
+    def undo_move(self):
+        """
+        Deshacer ultimo movimiento
+        """
+        if len(self.move_log) != 0:
+            move = self.move_log.pop()
+            self.board[move.end_r][move.end_c] = move.piece_capture
+            self.board[move.start_r][move.start_c] = move.piece_moved
+            self.white_turn = not self.white_turn
 
-    def get_valid_moves(self):pass
+    def get_valid_moves(self):
+        """
+        movimientos considerando jaques
+        """
+        return self.get_possible_moves()
 
     def check(self):pass
 
     def square_attacked(self):pass
 
-    def get_possible_moves(self):pass
+    def get_possible_moves(self):
+        """
+        movimientos sin considerar jaques
+        """
+        moves = []
+        for r in range(len(self.board)):
+            for c in range(len(self.board[r])):
+                turn, piece = self.board[r][c][0], self.board[r][c][1]
+                if (turn == "w" and self.white_turn) and (turn == "b" and not self.white_turn):
+                    match piece:
+                        case "P":
+                            self.pawn_moves(r, c, moves)
+                        case "R":
+                            self.rook_moves(r, c, moves)
+                        case "N":
+                            self.knight_moves(r, c, moves)
+                        case "B":
+                            self.bishop_moves(r, c, moves)
+                        case "Q":
+                            self.queen_moves(r, c, moves)
+                        case "K":
+                            self.king_moves(r, c, moves)
+        return moves
 
-    def pawn_moves(self):pass
+    def pawn_moves(self, r, c, moves):
+        """
+        obtener movimientos del peon r, c
+        :param r: fila
+        :param c: columna
+        :param moves: lista de movimientos
+        :return: movimientos del peon
+        """
 
-    def rook_moves(self):pass
+    def rook_moves(self, r, c, moves):
+        """
+        obtener movimientos de la torre r, c
+        :param r: fila
+        :param c: columna
+        :param moves: lista de movimientos
+        :return: movimientos de la torre
+        """
 
-    def knight_moves(self):pass
+    def knight_moves(self, r, c, moves):
+        """
+        obtener movimientos del caballero r, c
+        :param r: fila
+        :param c: columna
+        :param moves: lista de movimientos
+        :return: movimientos del caballero
+        """
 
-    def bishop_moves(self):pass
+    def bishop_moves(self, r, c, moves):
+        """
+        obtener movimientos del alfin r, c
+        :param r: fila
+        :param c: columna
+        :param moves: lista de movimientos
+        :return: movimientos del alfin
+        """
 
-    def queen_moves(self):pass
+    def queen_moves(self, r, c, moves):
+        """
+        obtener movimientos de la reina r, c
+        :param r: fila
+        :param c: columna
+        :param moves: lista de movimientos
+        :return: movimientos de la reina
+        """
 
-    def king_moves(self):pass
+    def king_moves(self, r, c, moves):
+        """
+        obtener movimientos del rey r, c
+        :param r: fila
+        :param c: columna
+        :param moves: lista de movimientos
+        :return: movimientos del rey
+        """
 
 
 class Move:
@@ -54,18 +137,43 @@ class Move:
     cols_files = {v: k for k, v in files_cols.items()}
 
     def __init__(self, start, end, board):
+        """
+        Crea un movimiento
+        :param start: primer click
+        :param end: segundo click
+        :param board: estado del tablero actual
+        """
         self.start_r = start[0]
         self.start_c = start[1]
         self.end_r = end[0]
         self.end_c = end[1]
         self.piece_moved = board[self.start_r][self.start_c]
-        self.piece_capture = board[self.end_r][self.start_c]
+        self.piece_capture = board[self.end_r][self.end_c]
+
+        self.move_id = self.start_r*1000 + self.start_c*100 + self.end_r*10 + self.end_c
 
     def get_notation(self):
+        """
+        :return: notacion del ajedrez del movimiento
+        """
         return self.get_rank_file(self.start_r, self.start_c) +\
             self.get_rank_file(self.end_r, self.end_c)
 
     def get_rank_file(self, r, c):
+        """
+        :param r: fila
+        :param c: columna
+        :return: notacion de ajedrez de la casilla
+        """
         return self.cols_files[c] + self.rows_ranks[r]
 
-    def __eq__(self, other):pass
+    def __eq__(self, other):
+        """
+        sobreescribir el metodo de igualdad
+        :param other: objeto a igualar
+        :return: bollean
+        """
+        if isinstance(other, Move):
+            return other.move_id == self.move_id
+        return False
+
--- game.py
+++ game.py
@@ -11,17 +11,26 @@ class Game:
         self.screen.fill("White")
         self.clock = pg.time.Clock()
         self.game_state = GameState()
+        self.valid_moves = self.game_state.get_valid_moves()
+        self.move_made = False
         self.running = True
 
         self.selected = ()
         self.clicks = []
 
+
     def load_images(self):
+        """
+        carga imagenes necesarias para el juego
+        """
         pieces = ["bP", "bR", "bB", "bN", "bQ", "bK", "wP", "wR", "wB", "wN", "wQ", "wK"]
         for piece in pieces:
             self.images[piece] = pg.image.load(DIR+piece+".png")
 
     def check_events(self):
+        """
+        verifica eventos del teclado/mouse
+        """
         for e in pg.event.get():
             if e.type == pg.QUIT:
                 self.running = False
@@ -37,12 +46,25 @@ class Game:
                     self.clicks.append(self.selected)
                 if len(self.clicks) == 2:
                     move = Move((self.clicks[0]), (self.clicks[1]), self.game_state.board)
-                    self.game_state.make_move(move)
-                    print(move.get_notation())
+                    if move in self.valid_moves:
+                        self.game_state.make_move(move)
+                        self.move_made = True
+                    print(f"{move.get_notation()} - {move.move_id}")
                     self.selected = ()
                     self.clicks = []
+            elif e.type == pg.KEYDOWN:
+                if e.key == pg.K_LCTRL:
+                    self.game_state.undo_move()
+                    self.move_made = True
+
+        if self.move_made:
+            self.valid_moves = self.game_state.get_valid_moves()
+            self.move_made = False
 
     def draw_board(self):
+        """
+        dibuja tablero
+        """
         colors = [pg.Color(DARK), pg.Color(LIGHT)]
         for r in range(DIMS):
             for c in range(DIMS):
@@ -52,6 +74,9 @@ class Game:
                 self.screen.blit(cas, (r*SQ_SIZE, c*SQ_SIZE))
 
     def draw_pieces(self):
+        """
+        dibuja las piezas segun el estado del tablero actual
+        """
         for r in range(DIMS):
             for c in range(DIMS):
                 piece = self.game_state.board[r][c]
@@ -61,11 +86,17 @@ class Game:
     def draw_highlight(self):pass
 
     def draw_game_state(self):
+        """
+        dibuja el estado del juego actual
+        """
         self.draw_board()
         self.draw_highlight()
         self.draw_pieces()
 
     def main(self):
+        """
+        funcion principal del juego
+        """
         pg.init()
         self.load_images()
 
